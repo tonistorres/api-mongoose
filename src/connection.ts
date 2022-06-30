102673d6115670c2edd7396bee84bc6d5347a452
tonistorres <sisvenda2011@gmail.com>
2022-06-29 21:27:27 -0300

    feat: implementado a classe de conexão

---
+++ src/connection.ts
@@ -0,0 +1,16 @@
+// imortando mongoose para dentro do projeto
+import mongoose from 'mongoose';
+
+/**Criando uma classe de conexão  */
+export default class MongoConnection {
+    /**Craindo um método estatico assincrono com nome de connect que recebe como 
+     * parâmetro uma URI do tipo String que nada mais é que a proprio caminho ou 
+     * link de conexão com o banco de dados mongodb por meio da porta padrão em 
+     * nosso caso 27017, tenho já na minha URI um valor default setado o que nada 
+     * me emprede no momento que instânciar esse métode eu possa passar uma outra 
+     * conexão
+     */
+    static async connect(URI: string = 'mongodb://localhost:27017/trybeflix') {
+        await mongoose.connect(URI);
+    }
+}
